0ace0507e440275db45c0afd893bc307cae10774
Attila Satan <attila.satan@gmail.com>
2018-06-04 01:32:49 +0300

    definitions

---
+++ definitions.go
@@ -0,0 +1,74 @@
+package pokedex
+
+type Type struct {
+	// Name of the type
+	Name string `json:"name"`
+	// The effective types, damage multiplize 2x
+	EffectiveAgainst []string `json:"effectiveAgainst"`
+	// The weak types that against, damage multiplize 0.5x
+	WeakAgainst []string `json:"weakAgainst"`
+}
+
+type Pokemon struct {
+	Number         string   `json:"Number"`
+	Name           string   `json:"Name"`
+	Classification string   `json:"Classification"`
+	TypeI          []string `json:"Type I"`
+	TypeII         []string `json:"Type II,omitempty"`
+	Weaknesses     []string `json:"Weaknesses"`
+	FastAttackS    []string `json:"Fast Attack(s)"`
+	Weight         string   `json:"Weight"`
+	Height         string   `json:"Height"`
+	Candy          struct {
+		Name     string `json:"Name"`
+		FamilyID int    `json:"FamilyID"`
+	} `json:"Candy"`
+	NextEvolutionRequirements struct {
+		Amount int    `json:"Amount"`
+		Family int    `json:"Family"`
+		Name   string `json:"Name"`
+	} `json:"Next Evolution Requirements,omitempty"`
+	NextEvolutions []struct {
+		Number string `json:"Number"`
+		Name   string `json:"Name"`
+	} `json:"Next evolution(s),omitempty"`
+	PreviousEvolutions []struct {
+		Number string `json:"Number"`
+		Name   string `json:"Name"`
+	} `json:"Previous evolution(s),omitempty"`
+	SpecialAttacks      []string `json:"Special Attack(s)"`
+	BaseAttack          int      `json:"BaseAttack"`
+	BaseDefense         int      `json:"BaseDefense"`
+	BaseStamina         int      `json:"BaseStamina"`
+	CaptureRate         float64  `json:"CaptureRate"`
+	FleeRate            float64  `json:"FleeRate"`
+	BuddyDistanceNeeded int      `json:"BuddyDistanceNeeded"`
+}
+
+// Move is an attack information. The
+type Move struct {
+	// The ID of the move
+	ID int `json:"id"`
+	// Name of the attack
+	Name string `json:"name"`
+	// Type of attack
+	Type string `json:"type"`
+	// The damage that enemy will take
+	Damage int `json:"damage"`
+	// Energy requirement of the attack
+	Energy int `json:"energy"`
+	// Dps is Damage Per Second
+	Dps float64 `json:"dps"`
+	// The duration
+	Duration int `json:"duration"`
+}
+
+// BaseData is a struct for reading data.json
+type BaseData struct {
+	Types    []Type    `json:"types"`
+	Pokemons []Pokemon `json:"pokemons"`
+	Moves    []Move    `json:"moves"`
+}
+
+// PokemonIndexByName is a map for pokemons by their name as keys
+var PokemonIndexByName map[string]int = make(map[string]int)
